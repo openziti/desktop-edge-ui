874d8a703cf516a31f6d94662c5e24bbd36098e2
Jeremy Tellier <jeremy.tellier@netfoundry.io>
2023-12-13 10:39:56 -0500

    Fix output

--- build.ps1
+++ build.ps1
@@ -20,7 +20,7 @@ if (test-path 'release-builds') {
     echo "Releases Exist, Removing"
     Remove-Item release-builds -Recurse -Force -Confirm:$false
 }
-electron-packager ./ziti-edge-ui Ziti-Desktop-Edge --overwrite --asar --electron-version=26.2.4 --platform=win32 --arch=x64 --prune=true  --out=./release-builds --icon=./app.ico
+electron-packager ./ziti-edge-ui Ziti-Desktop-Edge --overwrite --asar --electron-version=26.2.4 --platform=win32 --arch=x64 --prune=true  --out=./release-builds --icon=./ziti-edge-ui/app.ico
 
 echo "------Compress Build Files------\n\n"
 Compress-Archive -Path ./release-builds/Ziti-Desktop-Edge-win32-x64/* -DestinationPath ./release-builds/ZitiUI.zip
