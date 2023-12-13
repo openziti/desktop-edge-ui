3f0fb35847db2522dfc530fb8b1b2c0cc586b465
Jeremy Tellier <jeremy.tellier@netfoundry.io>
2023-12-13 11:10:14 -0500

    Rev Up Electron

--- linux-pack.sh
+++ linux-pack.sh
@@ -1,1 +1,1 @@
-electron-packager ./ziti-edge-ui ziti-desktop-edge --overwrite --asar --electron-version=21.4.3 --platform=linux --arch=x64 --prune=true --out=release-builds --icon=./ziti-edge-ui/app.ico
+electron-packager ./ziti-edge-ui ziti-desktop-edge --overwrite --asar --electron-version=26.2.4 --platform=linux --arch=x64 --prune=true --out=release-builds --icon=./ziti-edge-ui/app.ico
--- win-pack.bat
+++ win-pack.bat
@@ -1,1 +1,1 @@
-electron-packager ./ziti-edge-ui/src Ziti-Desktop-Edge --overwrite --asar --electron-version=21.4.3 --platform=win32 --arch=x64 --prune=true --out=release-builds --icon=./ziti-edge-ui/app.ico
+electron-packager ./ziti-edge-ui/src Ziti-Desktop-Edge --overwrite --asar --electron-version=26.2.4 --platform=win32 --arch=x64 --prune=true --out=release-builds --icon=./ziti-edge-ui/app.ico
